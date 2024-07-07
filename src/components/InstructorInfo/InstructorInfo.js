8607aef0845b58a7f5cccee86d377689328b78e4
Suryansh Singh <supremesofficialyt@gmail.com>
2024-07-08 00:40:33 +0530

    Created Instructor Info Component

--- src/components/InstructorInfo/InstructorInfo.js
+++ src/components/InstructorInfo/InstructorInfo.js
@@ -1,10 +1,27 @@
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import images from "../images";
 import "./InstructorInfo.css";
 
-import React from "react"
-
 const InstructorInfo = () => {
+
+    useEffect(() => {
+        // Select all images and apply the opacity animation
+        gsap.from("img", {
+            duration: 1,
+            opacity: 0,
+            stagger: 0.2
+        });
+    }, []);
+
     return (
-        <div></div>
+        <div>
+            <h1 className="heading">Our Instructors</h1>
+            <img className="css-margin" src={images.instructorDemoImage1} alt="" />
+            <img src={images.instructorDemoImage2} alt="" />
+            <img src={images.instructorDemoImage3} alt="" />
+            <img src={images.instructorDemoImage4} alt="" />
+        </div>
     )
 }
 
--- src/components/images.js
+++ src/components/images.js
@@ -6,7 +6,11 @@ import physics from "../assests/images/Subjects/physics.jpg";
 import english from "../assests/images/Subjects/english.jpg";
 import science from "../assests/images/Subjects/science.jpg";
 import computer from "../assests/images/Subjects/computer.jpg";
+import instructorDemoImage1 from "../assests/images/Instructors/instructorDemoImage1.jpg";
+import instructorDemoImage2 from "../assests/images/Instructors/instructorDemoImage2.jpg";
+import instructorDemoImage3 from "../assests/images/Instructors/instructorDemoImage3.jpg";
+import instructorDemoImage4 from "../assests/images/Instructors/instructorDemoImage4.jpg";
 
-const images = {history, chemistry, math, biology, physics, english, science, computer};
+const images = {history, chemistry, math, biology, physics, english, science, computer, instructorDemoImage1, instructorDemoImage2, instructorDemoImage3, instructorDemoImage4};
 
 export default images;
